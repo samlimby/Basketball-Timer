71c8030e8fd903b2674e422a28f7c8e69ce49323
samlimby <129619263+samlimby@users.noreply.github.com>
2024-06-27 08:45:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let score = 0;
+document.getElementById("leftScore").textContent = score;
+
+
+let plus1 = 1;
+document.getElementById("plus1") = plus1
+let plus2 = 2;
+let plus3 = 3;
+
+function leftPlus1 () {
+    let leftScore = score += plus1;
+    labelElement.innerText = leftScore;
+    console.log(leftScore)
+
+}
+
