24afcec84150125c05659da14530946f0833c4a8
samlimby <129619263+samlimby@users.noreply.github.com>
2024-06-29 10:54:18 +0100

    v1 build

--- script.js
+++ script.js
@@ -1,15 +1,69 @@
 let countLeft = document.getElementById("left-score")
-console.log(countLeft)
+let resetLeft = document.getElementById("reset-left")
+let countRight = document.getElementById("right-score")
+let resetRight = document.getElementById("reset-right")
 
-leftScore = 5
+leftScore = 0
+rightScore = 0
 
-let plus1 = 1
-let plus2 = 2
-let plus3 = 3
+let leftPlus1 = 1
+let leftPlus2 = 2
+let leftPlus3 = 3
+let rightPlus1 = 1
+let rightPlus2 = 2
+let rightPlus3 = 3
 
-function logLeftScore () {
+function logLeftScore1 () {
     console.log("the button was clicked")
     leftScore = leftScore + 1;
     countLeft.innerText = leftScore
 
+}
+
+function logLeftScore2 () {
+    console.log("the button was clicked")
+    leftScore = leftScore + 2;
+    countLeft.innerText = leftScore
+
+}
+
+function logLeftScore3 () {
+    console.log("the button was clicked")
+    leftScore = leftScore + 3;
+    countLeft.innerText = leftScore
+
+}
+
+function resetLeftScore () {
+    console.log("the button was clicked")
+    leftScore = 0
+    countLeft.innerText = leftScore
+}
+
+function logRightScore1 () {
+    console.log("the button was clicked")
+    rightScore = rightScore + 1;
+    countRight.innerText = rightScore
+
+}
+
+function logRightScore2 () {
+    console.log("the button was clicked")
+    rightScore = rightScore + 2;
+    countRight.innerText = rightScore
+
+}
+
+function logRightScore3 () {
+    console.log("the button was clicked")
+    rightScore = rightScore + 3;
+    countRight.innerText = rightScore
+
+}
+
+
+function resetRightScore () {
+    console.log("the button was clicked")
+    rightScore = 0
+    countRight.innerText = rightScore
 }
